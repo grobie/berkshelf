56fb79b0d45d5a9febf14656ba4fcc98c73885b3
Tobias Schmidt <ts@soundcloud.com>
2013-10-29 17:38:06 +0530

    Fix extraction of git tar archives
    Git created tar archives include a pax_global_header as extended
    headers. Some tar versions, including libarchive, extract that header as
    regular file.
    
    This change ignores all files and uses the first directory.

--- lib/berkshelf/community_rest.rb
+++ lib/berkshelf/community_rest.rb
@@ -93,7 +93,7 @@ module Berkshelf
     def download(name, version)
       archive   = stream(find(name, version)[:file])
       extracted = self.class.unpack(archive.path)
-      Dir.glob(File.join(extracted, "*")).first
+      Dir.glob(File.join(extracted, "*")).detect { |f| File.directory?(f) }
     ensure
       archive.unlink unless archive.nil?
     end
